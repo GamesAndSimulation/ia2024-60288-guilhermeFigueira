260703366d909853d74799e53ef0079b08a753ad
Guilherme Figueira <grfigueira@10-22-154-60.ed3.eduroam.fct.unl.pt>
2024-03-26 10:17:14 +0000

    Teleport working

--- Assets/Teleporter.cs
+++ Assets/Teleporter.cs
@@ -8,15 +8,16 @@ public class Teleporter : MonoBehaviour
     {
         if (other.gameObject.tag == "Player")
         {
+            Debug.Log("Teleporting");
             foreach(Transform child in transform)
             {
                 if (child.gameObject.activeSelf)
                 {
                     Vector3 teleporterPosition = child.GetComponent<TeleporterArrrow>().pointToTeleporter.transform.position;
-                    teleporterPosition.y += 23;
-                    other.gameObject.transform.position = child.GetComponent<TeleporterArrrow>().pointToTeleporter.transform.position;
+                    teleporterPosition.y += 40;
+                    other.gameObject.transform.position = teleporterPosition;
                     Rigidbody otherRB = other.GetComponent<Rigidbody>();
-                    otherRB.AddForce(Vector3.up * 40000, ForceMode.Impulse);
+                    otherRB.AddForce(Vector3.up * 7500, ForceMode.Force);
 
                     break;
                 }
