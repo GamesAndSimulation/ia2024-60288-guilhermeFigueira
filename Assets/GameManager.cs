36956435c1908dfda2291e7fe82791256864f0aa
Guilherme Figueira <g.figueira.2002@gmail.com>
2024-04-06 12:57:48 +0100

    done starting elevator

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,7 +1,10 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
